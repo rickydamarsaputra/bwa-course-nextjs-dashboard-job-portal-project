f497742a5d4648fd2773c965512a930429166e2c
rickydamarsaputra <retrocode.rc@gmail.com>
2024-09-18 10:13:35 +0700

    Slicing Sidebar Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Sidebar from "@/components/layouts/Sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,23 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main>
+          <div className="border-t">
+            <div className="bg-background">
+              <div className="flex flex-row">
+                <div className="hidden lg:block w-[18%]">
+                  <Sidebar/>
+                </div>
+                <div className="col-span-3 overflow-auto lg-col-span-5 lg:border-l w-[82%]">
+                  <div className="px-6 py-6 lg:px-8">
+                    <div>Header</div>
+                    {children}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
       </body>
     </html>
   );
---
+++ src/components/layouts/Sidebar/index.tsx
@@ -0,0 +1,72 @@
+import { Button } from "@/components/ui/button";
+import React, {FC} from "react";
+import { AiOutlineHome, AiOutlineMessage } from "react-icons/ai";
+import { FaRegBuilding } from "react-icons/fa";
+import { PiUsersThree } from "react-icons/pi";
+import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
+import { CiCalendar } from "react-icons/ci";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoIosLogOut } from "react-icons/io";
+
+interface SidebarProps{
+
+}
+
+const Sidebar: FC<SidebarProps> = ({})=>{
+  return(
+    <div className="pb-12 min-h-screen">
+      <div className="space-y-4 py-4">
+        <div className="px-3 py-2">
+          <h2 className="mb-2 px-4 text-lg font-semibold">
+            Dashboard
+          </h2>
+          <div className="space-y-3">
+            <Button variant={'ghost'} className="w-full justify-start rounded-none hover:text-primary">
+              <AiOutlineHome className="mr-4 text-lg"/>
+              Home
+            </Button>
+            <Button variant={'ghost'} className="w-full justify-start rounded-none hover:text-primary">
+              <AiOutlineMessage className="mr-4 text-lg"/>
+              Messages
+            </Button>
+            <Button variant={'ghost'} className="w-full justify-start rounded-none hover:text-primary">
+              <FaRegBuilding className="mr-4 text-lg"/>
+              Company Profile
+            </Button>
+            <Button variant={'ghost'} className="w-full justify-start rounded-none hover:text-primary">
+              <PiUsersThree className="mr-4 text-lg"/>
+              All Aplicants
+            </Button>
+            <Button variant={'ghost'} className="w-full justify-start rounded-none hover:text-primary">
+              <HiOutlineClipboardDocumentList className="mr-4 text-lg"/>
+              Job Listing
+            </Button>
+            <Button variant={'ghost'} className="w-full justify-start rounded-none hover:text-primary">
+              <CiCalendar className="mr-4 text-lg"/>
+              My Schedule
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="space-y-4 py-4">
+          <div className="px-3 py-2">
+            <h2 className="mb-2 px-4 text-lg font-semibold">
+              Settings
+            </h2>
+            <div className="space-y-3">
+              <Button variant={'ghost'} className="w-full justify-start rounded-none hover:text-primary">
+                <IoSettingsOutline className="mr-4 text-lg"/>
+                Settings
+              </Button>
+              <Button variant={'ghost'} className="w-full justify-start rounded-none text-red-500 hover:text-red-500 hover:bg-red-50">
+                <IoIosLogOut className="mr-4 text-lg"/>
+                Logout
+              </Button>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default Sidebar;
