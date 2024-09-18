7141ea8a797f962d2b269d80f1109009e95a1152
rickydamarsaputra <retrocode.rc@gmail.com>
2024-09-18 10:27:50 +0700

    Slicing Header Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import {Epilogue} from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/layouts/Sidebar";
+import Header from "@/components/layouts/Header";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const epilogue = Epilogue({subsets: ['latin']});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={epilogue.className}
       >
         <main>
           <div className="border-t">
@@ -38,7 +30,7 @@ export default function RootLayout({
                 </div>
                 <div className="col-span-3 overflow-auto lg-col-span-5 lg:border-l w-[82%]">
                   <div className="px-6 py-6 lg:px-8">
-                    <div>Header</div>
+                    <Header/>
                     {children}
                   </div>
                 </div>
