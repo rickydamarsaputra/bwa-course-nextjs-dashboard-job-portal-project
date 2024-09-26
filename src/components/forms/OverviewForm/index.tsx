0d0dc1559489fdf8404efb69ce5a2af88a300d97
rickydamarsaputra <retrocode.rc@gmail.com>
2024-09-26 20:25:11 +0700

    Slicing Settings Overview Component 2

--- src/components/forms/OverviewForm/index.tsx
+++ src/components/forms/OverviewForm/index.tsx
@@ -1,7 +1,13 @@
 "use client"
 
 import TitleForm from "@/components/atoms/TitleForm";
+import CustomUpload from "@/components/organisms/CustomUpload";
+import FieldInput from "@/components/organisms/FieldInput";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
+import { LOCATION_OPTIONS, optionType } from "@/constants";
 import { overviewFormSchema } from "@/lib/form-schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React, {FC} from "react";
@@ -26,7 +32,67 @@ const OverviewForm: FC<OverviewFormProps> = ({})=>{
         <div className="my-5">
           <TitleForm title="Basic Information" subtitle="This is company information that you can update anytime."/>
         </div>
-        <Separator/>
+        <Separator className="mb-7"/>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-7">
+            <FieldInput title="Company Logo" subTitle="This image will be shown publicly as company">
+              <CustomUpload form={form} name="image"/>
+            </FieldInput>
+            <FieldInput title="Company Details" subTitle="Introduce your company core info quickly to users by fill up company details">
+              <div className="space-y-5">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({field}) => (
+                    <FormItem>
+                      <FormLabel>Company Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Twitter" className="w-[450px]" {...field}/>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="website"
+                  render={({field}) => (
+                    <FormItem>
+                      <FormLabel>Website</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Twitter" className="w-[450px]" {...field}/>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="location"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Location</FormLabel>
+                      <Select onValueChange={field.onChange} defaultValue={field.value}>
+                        <FormControl>
+                          <SelectTrigger className="w-[450px]">
+                            <SelectValue placeholder="Select location" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {LOCATION_OPTIONS.map((item: optionType, i: number) => (
+                            <SelectItem key={item.id+i} value={item.id}>
+                              {item.label}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </FieldInput>
+          </form>
+        </Form>
     </div>
   );
 }
