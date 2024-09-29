88577ca60dd5cfb34c429779adba675cab52e7ad
rickydamarsaputra <retrocode.rc@gmail.com>
2024-09-29 20:11:03 +0700

    Slicing Settings Teams Component 3

--- src/components/forms/TeamForm/DialogAddTeam/index.tsx
+++ src/components/forms/TeamForm/DialogAddTeam/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, {FC} from "react";
 import {
   Dialog,
@@ -13,6 +15,9 @@ import { useForm } from "react-hook-form";
 import { teamFormSchema } from "@/lib/form-schema";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Separator } from "@/components/ui/separator";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 
 interface DialogAddTeamProps{
@@ -24,6 +29,10 @@ const DialogAddTeam: FC<DialogAddTeamProps> = ({})=>{
     resolver: zodResolver(teamFormSchema),
   });
 
+  const onSubmit = (val: z.infer<typeof teamFormSchema>) => {
+    console.log(val);
+  }
+
   return(
     <Dialog>
       <DialogTrigger asChild>
@@ -34,11 +43,70 @@ const DialogAddTeam: FC<DialogAddTeamProps> = ({})=>{
       </DialogTrigger>
       <DialogContent className="bg-white">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>Add new team</DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your account
-            and remove your data from our servers.
+            Fill the field to add new team
           </DialogDescription>
+          <Separator />
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Name" {...field}/>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="position"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Position</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Position" {...field}/>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="grid grid-cols-2 gap-2">
+                <FormField
+                  control={form.control}
+                  name="instagram"
+                  render={({field}) => (
+                    <FormItem>
+                      <FormLabel>Instagram</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Instagram" {...field}/>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="linkedin"
+                  render={({field}) => (
+                    <FormItem>
+                      <FormLabel>Linkedin</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Linkedin" {...field}/>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button type="submit">Save</Button>
+            </form>
+          </Form>
         </DialogHeader>
       </DialogContent>
     </Dialog>
