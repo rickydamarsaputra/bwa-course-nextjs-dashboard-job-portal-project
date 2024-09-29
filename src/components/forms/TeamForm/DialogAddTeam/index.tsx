972d4d6421d0ef41bab2327a1702d6d37fb18b2b
rickydamarsaputra <retrocode.rc@gmail.com>
2024-09-29 20:01:39 +0700

    Slicing Settings Teams Component 2

---
+++ src/components/forms/TeamForm/DialogAddTeam/index.tsx
@@ -0,0 +1,48 @@
+import React, {FC} from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { teamFormSchema } from "@/lib/form-schema";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+
+interface DialogAddTeamProps{
+
+}
+
+const DialogAddTeam: FC<DialogAddTeamProps> = ({})=>{
+  const form = useForm<z.infer<typeof teamFormSchema>>({
+    resolver: zodResolver(teamFormSchema),
+  });
+
+  return(
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <PlusIcon className="h-4 w-4 mr-2"/>
+          Add member
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="bg-white">
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your account
+            and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default DialogAddTeam;
