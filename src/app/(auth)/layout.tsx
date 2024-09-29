88462c0345f60c0ad361805af3eb35db5afc2562
rickydamarsaputra <retrocode.rc@gmail.com>
2024-09-29 20:38:18 +0700

    Slicing Organize Layout for auth page

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import {Epilogue} from "next/font/google";
+import "../globals.css";
+
+const epilogue = Epilogue({subsets: ['latin']});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={epilogue.className}
+      >
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
