01755a399e4e8052596f9cc34beb929e61f19cea
rickydamarsaputra <retrocode.rc@gmail.com>
2024-10-02 17:30:20 +0700

    Integration Sign In 1

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,3 +10,8 @@ export const hashPassword = async (password: string) => {
   const hashedPassword = await bcrypt.hash(password, 8);
   return hashedPassword;
 }
+
+export const comparePassword = async (password: string, hashedPassword: string) => {
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+  return isMatch;
+}
